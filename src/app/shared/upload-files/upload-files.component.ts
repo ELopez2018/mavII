2096fcb9de6764369d6df0a9ee402c3f88a26a14
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2021-04-21 15:23:29 -0500

    Estilos

--- src/app/shared/upload-files/upload-files.component.ts
+++ src/app/shared/upload-files/upload-files.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-upload-files',
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./upload-files.component.scss']
 })
 export class UploadFilesComponent implements OnInit {
-
+  @Input() subTitleForm: string ="Expediente";
   constructor() { }
 
   ngOnInit(): void {
