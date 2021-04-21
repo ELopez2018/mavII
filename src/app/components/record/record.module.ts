4e9a0a03a1b023e72c056f8dc1a11ef343589fe1
Estarlin Lopez <45240343+ELopez2018@users.noreply.github.com>
2021-04-21 09:03:46 -0500

    Componente Modal y Upload Files

--- src/app/components/record/record.module.ts
+++ src/app/components/record/record.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { RecordRoutingModule } from './record-routing.module';
 import { RecordComponent } from './record.component';
+import { SharedModule } from '@root/shared/shared.module';
 
 
 @NgModule({
   declarations: [RecordComponent],
   imports: [
     CommonModule,
-    RecordRoutingModule
+    RecordRoutingModule,
+    SharedModule
   ]
 })
 export class RecordModule { }
